7246fbbd81456f51de38fec6a69715d8de156899
Noxvel <noxvel101@gmail.com>
2015-09-28 23:56:36 +0300

    Initial Commit

---
+++ BreakToGo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BreakToGo
+//
+//  Created by Стас on 9/28/15.
+//  Copyright © 2015 Younga. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
